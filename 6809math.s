ca531840d7b1bd6b8f8ef5bfcc7a08f8cc9c8646
Daud Zoss <daudzoss@gmail.com>
2020-01-08 21:55:39 -0800

    peekdig() and get5bcd() fixed and commented

--- 6809math.s
+++ 6809math.s
@@ -3,7 +3,7 @@ d0to199	lda	#$0a	; 2	;uint16_t d0to199(uint1_t c,  // 100's
 	bcc	1f	; 3	;                 uint8_t b,   // 10's
 	addb	#$0a	; 2	;                 uint16_t x) { // 1's
 1	mul		;11	; uint16_t d = 10*(c*10 + b); c = d & 0x80;
-	abx		; 2	; return x += d & 0xff; // = c*100 + b*10
+	abx		; 2	; return x += d & 0xff; // = c*100 + b*10;
 	rts		; 2 (22);} // d0to199()
 
 ;;; convert a 3-digit signed BCD -128..127 to binary unsigned in X, signed in D
@@ -22,7 +22,7 @@ x10ind	stx	,--s	; 5	;int16_t x10inD(int16_t x) {
 	rola		; 2	;
 	aslb		; 2	;
 	rola		; 2	;
-	addd	,s++	; 9	; return d += d*4; // = x*10
+	addd	,s++	; 9	; return d += d*4; // = x*10;
 	rts		; 2 (35);} // x10inD()
 
 ;;; copy Y to D, convert a Y-digit unsigned BCD 0..32767 to binary unsigned in Y
@@ -54,52 +54,53 @@ d16ngtv	jsr	d0to32k	; 8(403); uint16_t x, d = y; // digit count y <= 5
 	exg	x,d	; 7	; return d, x = d0to32k(y, s) * (n ? -1 : 1);
 	rts		; 2(430);} // d16sgnd()
 
-;;; look ahead to next character in the stream
+;;; look ahead to next character in the array, plus one more if its the sign
 1	lda	,x		;
 	ldb	,+x		;
 	cmpb	#'0'		;
 	blo	3f		;
 	cmpb	#'0'		;
 	bhi	3f		;
-peekdig	ldb	,x		;peekdig(const char
-	cmpb	#'-'		;
+;	rts
+peekdig	ldb	,x		;char peekdig(const char** x, char* a/*zero*/) {
+	cmpb	#'-'		; char b = *(*x);
 	bne	2f		;
-	tsta			;
-	beq	1b		;
-	bne	4f		;
-2	cmpb	#'+'		;
-	bne	4f		;
-	tsta			;
-	beq	1b		;
-	bne	4f		;
-3	andb	#$c0		;
-	beq	4f		;
-	ldb	#$01		;
-4	rts			;
+	tsta			; if (b == '-' || b == '+') {
+	beq	1b		;  if (*a == '\0') { // first character found
+	bne	4f		;   *a = b; // gets stored in *a to remember '-'
+2	cmpb	#'+'		;   ++(*x); // then advance x pointer past it
+	bne	4f		;   if (*(*x) >= '0' && *(*x) <= '9')
+	tsta			;    b = peekdig(x, a); // = *(*x);
+	beq	1b		;   else
+	bne	4f		;    b = (b & 0xc0) ? /*implicit*/ 1 : 0/*err*/;
+3	andb	#$c0		;  }
+	beq	4f		; }
+	ldb	#$01		; return b;
+4	rts			;}
 	
-get5bcd	ldy	#$0000		;
-	clra			;
-1	jsr	peekdig		;
+get5bcd	ldy	#$0000		;int16_t get5bcd(const char** x) {
+	clra			; uint16_t y = 0, d;
+1	jsr	peekdig		; uint8_t a = 0, b, s[5];
 	cmpb	#'0'		;
-	blo	2f		;
-	cmpb	#'9'		;
-	bhi	2f		;
-	leax	1,x		;
-	leay	-5,y		;
-	beq	4f		;
+	blo	3f		; while (1) {
+	cmpb	#'9'		;  b = peekdig(x, a);
+	bhi	3f		;  if (b >= '0' && b <= '9') // verified a digit
+	leax	1,x		;   if ((*x)++ // now *x points to a known digit
+	leay	-5,y		;       &&
+	bne	2f		;       (y != 5)) {
 	leay	6,y		;
 	andb	#$0f		;
-	stb	,-s		;
-	bra			;
-
-2	tstb			;
-	beq	err		;
-	cmpa	#'-'		;
-	beq			;
-3	jsr	d0to32k		;
-	bra			;
-	jsr	d16ngtv		;
-	
-4	leas	d,s		; // b contains number of digits converted!
-	rts			;
+	stb	,-s		;    s[y++] = b - '0';
+	bra	1b		;    continue;
+2	clrb			;   } else
+	bra	5f		;    b = 0; // indicates unsuccessful conversion
+3	tstb			;  break;
+	beq	5f		; }
+	cmpa	#'-'		; if ((d = b))
+	beq			;  if (a != '-')
+	jsr	d0to32k		;   d = y, y = d0to32k(y, s); // x is preserved
+	bra			;  else
+4	jsr	d16ngtv		;   d = y, y = d16ngtv(y, s); // =-d0to32k(y,s);
+5	leas	d,s		; return b = d & 0x07; // total digits converted
+	rts			;}
 	
