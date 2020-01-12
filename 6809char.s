73d8b46368c4b126e0a3159e96fdfedf142e8613
Daud Zoss <daudzoss@gmail.com>
2020-01-11 16:20:45 -0800

    fixed critical error in length subtraction

--- 6809char.s
+++ 6809char.s
@@ -1,19 +1,19 @@
 eatspc	stx	,--s	; 9	;void eatspc(struct {uint8_t n; char* c;}* str){
 	ldb	,x+	; 	; uint8_t b = str->n;
-	tfr	x,y	; 	; char* y = str->c, a;
-	bra	2f	; 	;
+	tfr	x,y	; 7	; char* y = str->c, a;
+	bra	2f	; 3	;
 1	sta	,y+	; 	; for (char* x = y; b--; x++)
-2	decb		; 	;
-	bmi	3f	; 	;
+2	decb		; 2	;
+	bmi	3f	; 3	;
 	lda	,x+	; 	;
-	cmpa	#' '	; 	;
-	beq	2b	; 	;  if ((a = *x) != ' ')
-	bne	1b	; 	;   *y++ = a;
-3	tfr	y,d	; 	;
-	subb	,s	; 	; b = y - x; // negative delta in string size
+	cmpa	#' '	; 2	;
+	beq	2b	; 3	;  if ((a = *x) != ' ')
+	bne	1b	; 3	;   *y++ = a;
+3	tfr	y,d	; 7	;
+	subd	,s	; 	; b = y - x; // (negative) delta in string size
 	ldx	,s	; 	;
 	abx		; 	;
-	tfr	x,d	; 	;
+	tfr	x,d	; 7	;
 	ldx	,s++	; 	;
 	stb	,x	; 	; str->n += b; // less than or equal to original
-	rts		;	;} // eatspc()
+	rts		; 5	;} // eatspc()
