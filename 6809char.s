20a9d3086d744ecb1fbbbd1b8df8dd9c906acf7a
Daud Zoss <daudzoss@gmail.com>
2020-01-26 19:42:33 -0800

    fixed logical error in eatspc()

--- 6809char.s
+++ 6809char.s
@@ -1,20 +1,21 @@
 ;;; compress ' ' out of a length-prefixed string in situ, update new length byte
 eatspc	stx	,--s	; 9	;void eatspc(struct {uint8_t n; char* c;}* str){
 	ldb	,x+	; 6	; uint8_t b = str->n;
+	incb		; 2	;
 	tfr	x,y	; 6	; char* y = str->c, a;
 	bra	2f	; 3	;
 1	sta	,y+	; 6	; for (char* x = y; b--; x++)
 2	decb		; 2	;
-	bmi	3f	; 3	;
+	beq	3f	; 3	;
 	lda	,x+	; 6	;
 	cmpa	#' '	; 2	;
 	beq	2b	; 3	;  if ((a = *x) != ' ')
 	bne	1b	; 3	;   *y++ = a;
 3	tfr	y,d	; 6	;
-	subd	,s	; 6	; b = y - x; // (negative) delta in string size
-	addd	,s	; 6	;
+	subd	,s	; 6	; // x - (x - y) == x + (y - x) < x
+	subd	,s	; 6	; b = x - y; // (negative) delta in string size
 	ldx	,s++	; 8	;
-	stb	,x	; 4	; str->n += b; // less than or equal to original
+	stb	,x	; 4	; str->n -= b; // less than or equal to original
 	rts		;5(6169);} // eatspc()
 
 ;;; read a polynomial with int16_t coefficients, variables and uint2_t exponents
