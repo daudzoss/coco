38b5d95b78f052df17c2bee0420322d37f0da0ae
Daud Zoss <daudzoss@gmail.com>
2020-02-18 13:17:12 -0800

    changed initial guesss prefix from ',' to '@', traced through x2

--- 6809math.s
+++ 6809math.s
@@ -402,8 +402,8 @@ getpoly	cmpx	10,s	;	;int8_t getpoly(register char* x, int16_t s[5]){
 	tstb		;	;
 	beq	4f	;	;  if (d) { // successfully converted into Y
 	lda	,x+	;	;   char a = *x++; // expecting var, +, - or end
-	cmpa	#','	;	;
-	bne	1f	;	;   if (a == ',') {// comma before initial guess
+	cmpa	#'@'	;	;
+	bne	1f	;	;   if (a == '@') {// comma before initial guess
 	jsr	get5bcd	;	;    uint8_t b = get5bcd(&x, &y);
 	tstb		;	;    if (b == 0)
 	beq	4f	;	;     return -1;// no value provided after comma
