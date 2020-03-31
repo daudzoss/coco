b0d4364b5376adfd164034e6f33f9a74191a0593
Daud Zoss <daudzoss@gmail.com>
2020-03-30 18:46:02 -0700

    writing test suites

--- 6809test.s
+++ 6809test.s
@@ -1,23 +1,43 @@
-#if 0
+ if 0
 td0to19	orcc	#SEC		;1
-	ldb	#9		;9
+	ldd	#9		;9
 	ldx	#3		;3
 	jsr	d0to199		;+
 	tfr	x,y		;
 	andcc	#CLC		;
-	ldb	#0		;
+	ldd	#0		;
 	ldx	#7		;7
 	jsr	d0to199		;=
 	tfr	y,d		;
 	abx			;2
+	
 	ldy	#$9a55
 	cmpx	#$00c8		;0
 	beq	1f		;0
 	ldy	#$fa17		;F
 1	swi			;P
-#endif
+ endif
 
-#if 1
-td8sgnd	
-
-tx10ind	
+ if 0
+td8sgnd	orcc	#SEC		;1
+	ldd	#2		;2
+	ldx	#7		;7
+	orcc	#SEN		;-
+	jsr	d8sgnd		;+
+	abx			;
+	ldy	#$9a55		;
+	cmpx	#$0100		;
+	beq	1f		;
+	ldy	#$fa17		;F
+1	swi			;P
+ endif
+	
+ if 1
+tx10ind	ldx	#$0a		;
+	jsr	x10ind		;
+	ldy	#$9a55		;
+	cmpd	#$0064		;
+	beq	1f		;
+	ldy	#$fa17		;
+1	swi			;
+ endif
