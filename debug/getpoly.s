daf09d607c1ca5d1c59e699e71adf25545491ad8
Daud Zoss <daudzoss@gmail.com>
2020-05-02 21:57:24 -0700

    fixed test but the function under test crashes

--- debug/getpoly.s
+++ debug/getpoly.s
@@ -1,5 +1,5 @@
 ;;; getpoly.s
- if 1
+ if 0 ; // something rotten in the state of getpoly()
 chkpoly	macro
 	ldb	poly\1	; 2	;{
 	ldx	#poly\1	; 3	; char* x;
@@ -15,16 +15,16 @@ chkpoly	macro
 	jsr	getpoly	; 7 ( )	; getpoly(x, s);
 	ldy	#$fa17	;	;
 	ldd	3+coeff\1	;
-	cmp	,s++		; if (s[1] != coeff[0])
+	cmpd	,s++		; if (s[1] != coeff[0])
 	bne	0f		;  return y = 0xfa17; // faIL
 	ldd	2+coeff\1	;
-	cmp	,s++		; if (s[2] != coeff[1])
+	cmpd	,s++		; if (s[2] != coeff[1])
 	bne	0f		;  return y = 0xfa17; // faIL
 	ldd	1+coeff\1	;
-	cmp	,s++		; if (s[3] != coeff[3])
+	cmpd	,s++		; if (s[3] != coeff[3])
 	bne	0f		;  return y = 0xfa17; // faIL
 	ldd	0+coeff\1	;
-	cmp	,s++		; if (s[4] != coeff[4])
+	cmpd	,s++		; if (s[4] != coeff[4])
 	bne	0f		;  return y = 0xfa17; // faIL
 	ldy	#$9a55		; return y = 0x9a55; // PaSS
 0	leas	10,s		;}
@@ -43,16 +43,21 @@ poly1	fcb	poly2-poly1-1
 	fcc	"1x2-6x+9"
 poly2	fcb	poly3-poly2-1
 	fcc	"x2-7x+12"
-poly3
-
-coeff1	fcw	0
-	fcw	1
-	fcw	-6
-	fcw	9
-coeff2	fcw	0	
-	fcw	1
-	fcw	-7
-	fcw	12
-
-coeff3
+poly3	fcb	poly4-poly3-1
+	fcc	"8x3+1"
+poly4
+	
+coeff1	fdb	0
+	fdb	1
+	fdb	-6
+	fdb	9
+coeff2	fdb	0	
+	fdb	1
+	fdb	-7
+	fdb	12
+coeff3	fdb	8
+	fdb	0
+	fdb	0
+	fdb	1
+coeff4
  endif
