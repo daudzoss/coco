2feeb8f7e3986d5b4994514b4d724e6334b75ccb
Daud Zoss <daudzoss@gmail.com>
2020-05-02 22:09:58 -0700

    touch-ups to fix stack bug

--- debug/getpoly.s
+++ debug/getpoly.s
@@ -1,9 +1,9 @@
 ;;; getpoly.s
- if 0 ; // something rotten in the state of getpoly()
+ if 1 ; // something rotten in the state of getpoly()
 chkpoly	macro
-	ldb	poly\1	; 2	;{
+	ldb	poly\1	; 2	;uint16_t chkpoly(char poly[],uint16_t coeff[]){
 	ldx	#poly\1	; 3	; char* x;
-	leas	-10,s	; 5	; uint16_t s[5], y, coeff[4];
+	leas	-10,s	; 5	; uint16_t s[5], y;
 	abx		; 3	; // push the last character of string to stack:
 	stx	8,s	; 9	; s[4] = (uint16_t) (poly + (char*) *poly);
 	ldx	#0	; 3	;
@@ -13,21 +13,21 @@ chkpoly	macro
 	stx	0,s	; 6	; s[0] = 0; // x^0 coeff at stack pointer + 0
 	ldx	#1+poly\1;3	; x = &poly[1];// past size byte to string start
 	jsr	getpoly	; 7 ( )	; getpoly(x, s);
-	ldy	#$fa17	;	;
-	ldd	3+coeff\1	;
-	cmpd	,s++		; if (s[1] != coeff[0])
-	bne	0f		;  return y = 0xfa17; // faIL
-	ldd	2+coeff\1	;
-	cmpd	,s++		; if (s[2] != coeff[1])
-	bne	0f		;  return y = 0xfa17; // faIL
-	ldd	1+coeff\1	;
-	cmpd	,s++		; if (s[3] != coeff[3])
-	bne	0f		;  return y = 0xfa17; // faIL
-	ldd	0+coeff\1	;
-	cmpd	,s++		; if (s[4] != coeff[4])
-	bne	0f		;  return y = 0xfa17; // faIL
-	ldy	#$9a55		; return y = 0x9a55; // PaSS
-0	leas	10,s		;}
+	ldy	#$fa17	; 4	;
+	ldd	3+coeff\1;5	;
+	cmpd	3*2,s	; 8	; if (s[3] != coeff[3])
+	bne	0f	; 3	;  return y = 0xfa17; // faIL
+	ldd	2+coeff\1;5	;
+	cmpd	2*2,s	; 8	; if (s[2] != coeff[2])
+	bne	0f	; 3	;  return y = 0xfa17; // faIL
+	ldd	1+coeff\1;5	;
+	cmpd	1*2,s	; 8	; if (s[1] != coeff[1])
+	bne	0f	; 3	;  return y = 0xfa17; // faIL
+	ldd	0+coeff\1;5	;
+	cmpd	0*2,s	; 8	; if (s[0] != coeff[0])
+	bne	0f	; 3	;  return y = 0xfa17; // faIL
+	ldy	#$9a55	; 5	; return y = 0x9a55; // PaSS
+0	leas	10,s	; 5	;}
 	endm
 
 1	chkpoly	1
