fda5eef72b11e915dfad9a2c2d4a398dd3fe7080
Daud Zoss <daudzoss@gmail.com>
2020-05-05 10:21:38 -0700

    corrected getpoly.s but getply() not processing negative coefficient correctly

--- debug/getpoly.s
+++ debug/getpoly.s
@@ -1,6 +1,6 @@
 ;;; getpoly.s
  if 1 ; // something rotten in the state of getpoly()
-	bra	coeff4
+	jmp	coeff4
 chkpoly	macro
 	ldb	poly\1	; 2	;uint16_t chkpoly(char poly[],uint16_t coeff[]){
 	ldx	#poly\1	; 3	; char* x;
@@ -15,17 +15,17 @@ chkpoly	macro
 	ldx	#1+poly\1;3	; x = &poly[1];// past size byte to string start
 	jsr	getpoly	; 7 ( )	; getpoly(x, s);
 	ldy	#$fa17	; 4	;
-	ldd	3+coeff\1;5	;
-	cmpd	3*2,s	; 8	; if (s[3] != coeff[3])
+	ldd	6+coeff\1;5	;
+	cmpd	6,s	; 8	; if (s[3] != coeff[3])
+	bne	0f	; 3	;  return y = 0xfa17; // faIL
+	ldd	4+coeff\1;5	;
+	cmpd	4,s	; 8	; if (s[2] != coeff[2])
 	bne	0f	; 3	;  return y = 0xfa17; // faIL
 	ldd	2+coeff\1;5	;
-	cmpd	2*2,s	; 8	; if (s[2] != coeff[2])
-	bne	0f	; 3	;  return y = 0xfa17; // faIL
-	ldd	1+coeff\1;5	;
-	cmpd	1*2,s	; 8	; if (s[1] != coeff[1])
+	cmpd	2,s	; 8	; if (s[1] != coeff[1])
 	bne	0f	; 3	;  return y = 0xfa17; // faIL
 	ldd	0+coeff\1;5	;
-	cmpd	0*2,s	; 8	; if (s[0] != coeff[0])
+	cmpd	0,s	; 8	; if (s[0] != coeff[0])
 	bne	0f	; 3	;  return y = 0xfa17; // faIL
 	ldy	#$9a55	; 5	; return y = 0x9a55; // PaSS
 0	leas	10,s	; 5	;}
