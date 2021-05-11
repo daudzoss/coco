b516b8418bcf8ce4d60e5420e9578020381e2ef9
Daud Zoss <daudzoss@gmail.com>
2021-05-11 12:27:06 -0700

    tweaking stack cleanup

--- overbook/display.s
+++ overbook/display.s
@@ -1,3 +1,15 @@
+;;; X,Y = rotate(X,Y,D)
+;;; rotate a 2D nonnegative byte pattern 90 degrees (up to 4x4) or 180 (if Nx5)
+;;;
+;;;         16_15_14_13_12_11_10_ 9_ 8_ 7_ 6_ 5_ 4_ 3_ 2_ 1_ 0_
+;;; input:                                            [cols<=4] in X
+;;; input:                                            [rows<=5] in Y
+;;; input:  [       pointer to 20-byte array <= 65516         ] in D (A:B)
+;;; output:                                           [cols<=4] in X
+;;; output:                                           [rows<=5] in Y
+;;;
+;;; e.g.
+;;; X = 3
 rotate	pshs	y,x		;void rotate(uint16_t*x, uint16_t*y, uint_8* d){
 	tfr	d,x		;
 	lda	1,s		; uint8_t a = *x; // old width of card to rotate
@@ -5,7 +17,7 @@ rotate	pshs	y,x		;void rotate(uint16_t*x, uint16_t*y, uint_8* d){
 	cmpb	#4		;
 	bls	2f		; if (b > 4) { // too wide if rotated 90 degrees
 	tfr	x,y		;  uint8_t* x = d, * y = d;
-	mul			;  for (b = a * b - 1; b >= 0; b--) {
+	mul			;  for (uint8_t b = a * b - 1; b >= 0; b--) {
 1	decb			;
 	lda	b,x		;
 	sta	,-s		;   uint8_t s = x[b];
@@ -15,12 +27,13 @@ rotate	pshs	y,x		;void rotate(uint16_t*x, uint16_t*y, uint_8* d){
 	sta	,y+		;
 	tstb			;   *y++ = s;
 	bne	1b		;  } // reversing the array rotated it by 180
-	bra	end_of_function	;  return; // *x and *y dimensions unclobbered
+	bra	8f		;  // *x and *y dimensions unclobbered
+	
 2	tfr	pc,y		; } else { // rotate counter?clockwise by 90
 rotate2	leay	rotbuf-rotate2,y;
 	;ldy	#rotbuf
 	bra	3f		;
-rotbuf	rmb	5*4		;  static uint8_t rotbuf[4/*cols*/ * 5/*rows*/];
+rotbuf	rmb	5*4		;  static uint8_t rotbuf[5/*row*/ * 4/*seats*/];
 3	sta	3,s		;  *y = a; // new height
 	stb	1,s		;  *x = b; // new width
 	mul			;
@@ -30,14 +43,15 @@ rotbuf	rmb	5*4		;  static uint8_t rotbuf[4/*cols*/ * 5/*rows*/];
 	ldb	a,x		;
 	cmpa	,s		;
 	blo	5f		;
-	ldb	#-1		;
+	ldb	#$ff		;
 5	stb	a,y		;
 	tsta			;
 	bne	4b		;   rotbuf[a] = (a < s) ? x[a] : -1;
-	leay	d,y		;  for (uint8_t* y = &(rotbuf[a*b - 1]); s; y--)
+	
+	leay	d,y		;
 	ldb	1+1,s		;
-	abx			;
-	leax	-1,x		;   for (uint8_t* x = &(d[b-1]); *x>=0; x += b){
+	abx			;  for (uint8_t* y = &(rotbuf[a*b - 1]); s; y--)
+6	leax	-1,x		;   for (uint8_t* x = &(d[b-1]); *x>=0; x += b){
 	pshs	x		;
 7	dec	2,s		;    s--;
 	lda	,-y		;
@@ -46,14 +60,10 @@ rotbuf	rmb	5*4		;  static uint8_t rotbuf[4/*cols*/ * 5/*rows*/];
 	lda	,x		;    *x = *y;
 	bpl	7b		;   }
 	puls	x		;
-	leax	-1,x		;
 	tst	2,s		;
 	bne	6b		;
-
-;;; idea: use abx with x as a negative offset to move through buffer?
-
-end_of_function
-	puls	x,y		;
+	leas	1,s		; }
+8	puls	x,y		;// *x and *y dimensions updated (if 90 degrees)
 	rts			;} // rotate()
 
 toowide	abx			;toowide(uint8_t b, uint16_t* x) { // pos,width
